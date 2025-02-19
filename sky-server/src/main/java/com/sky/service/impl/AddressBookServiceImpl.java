9330bef2eb60eeba9269a15046322c7966ce79d1
a-fei <sure_0725@qq.com>
2025-02-19 20:42:51 +0800

    day08-地址薄功能模块

---
+++ sky-server/src/main/java/com/sky/service/impl/AddressBookServiceImpl.java
@@ -0,0 +1,89 @@
+package com.sky.service.impl;
+
+import com.sky.context.BaseContext;
+import com.sky.entity.AddressBook;
+import com.sky.mapper.AddressBookMapper;
+import com.sky.service.AddressBookService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+
+@Service
+@Slf4j
+public class AddressBookServiceImpl implements AddressBookService {
+    @Autowired
+    private AddressBookMapper addressBookMapper;
+
+    /**
+     * 条件查询
+     *
+     * @param addressBook
+     * @return
+     */
+    public List<AddressBook> list(AddressBook addressBook) {
+        return addressBookMapper.list(addressBook);
+    }
+
+    /**
+     * 新增地址
+     *
+     * @param addressBook
+     */
+    public void save(AddressBook addressBook) {
+        addressBook.setUserId(BaseContext.getCurrentId());
+        addressBook.setIsDefault(0);
+        addressBookMapper.insert(addressBook);
+    }
+
+    /**
+     * 根据id查询
+     *
+     * @param id
+     * @return
+     */
+    public AddressBook getById(Long id) {
+        AddressBook addressBook = addressBookMapper.getById(id);
+        return addressBook;
+    }
+
+    /**
+     * 根据id修改地址
+     *
+     * @param addressBook
+     */
+    public void update(AddressBook addressBook) {
+        //查看mapper.xml sql语句你会发现 这里省市区信息无法修改
+        //这样用户在别的信息无误，只有这几项信息有误的情况只能删掉重写...
+        //虽然不合理 但是这里就不深究了...
+        addressBookMapper.update(addressBook);
+    }
+
+    /**
+     * 设置默认地址
+     *
+     * @param addressBook
+     */
+    @Transactional
+    public void setDefault(AddressBook addressBook) {
+        //1、将当前用户的所有地址修改为非默认地址 update address_book set is_default = ? where user_id = ?
+        addressBook.setIsDefault(0);
+        addressBook.setUserId(BaseContext.getCurrentId());
+        addressBookMapper.updateIsDefaultByUserId(addressBook);
+
+        //2、将当前地址改为默认地址 update address_book set is_default = ? where id = ?
+        addressBook.setIsDefault(1);
+        addressBookMapper.update(addressBook);
+    }
+
+    /**
+     * 根据id删除地址
+     *
+     * @param id
+     */
+    public void deleteById(Long id) {
+        addressBookMapper.deleteById(id);
+    }
+
+}
