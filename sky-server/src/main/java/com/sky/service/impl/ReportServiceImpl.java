8c1d680629d27a9687162d4c6ef14a79156753ab
a-fei <sure_0725@qq.com>
2025-02-24 10:55:58 +0800

    day11-营业额统计

---
+++ sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
@@ -0,0 +1,67 @@
+package com.sky.service.impl;
+
+import com.sky.entity.Orders;
+import com.sky.mapper.ReportMapper;
+import com.sky.service.ReportService;
+import com.sky.vo.TurnoverReportVO;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class ReportServiceImpl implements ReportService {
+    @Autowired
+    private ReportMapper reportMapper;
+
+    /**
+     * 营业额统计接口
+     * @param begin
+     * @param end
+     * @return
+     */
+    @Override
+    public TurnoverReportVO getTurnoverStatistics(LocalDate begin, LocalDate end) {
+        List<LocalDate> dateList = new ArrayList<>();
+        dateList.add(begin);
+        //获取begin-end之间的List集合dateList
+        while(!begin.equals(end)){
+            begin = begin.plusDays(1);
+            dateList.add(begin);
+        }
+        //将dateList拼接转换为','分割的字符串
+        StringBuilder stringBuilder = new StringBuilder();
+        for (LocalDate date : dateList) {
+            if (stringBuilder.length() > 0) {
+                stringBuilder.append(",");
+            }
+            stringBuilder.append(date.toString());
+        }
+        String dateListString = stringBuilder.toString();
+        //计算每天营业额集合turnoverList
+        List<Double> turnoverList = new ArrayList<>();
+        for (LocalDate date : dateList) {
+            LocalDateTime beginTime = LocalDateTime.of(date, LocalTime.MIN);
+            LocalDateTime endTime = LocalDateTime.of(date, LocalTime.MAX);
+            Map map = new HashMap();
+            map.put("begin", beginTime);
+            map.put("end", endTime);
+            map.put("status", Orders.COMPLETED);
+            Double turnover = reportMapper.getDateSum(map);
+            if (turnover == null) {
+                turnover = 0.0;
+            }
+            turnoverList.add(turnover);
+        }
+        //这个是org.apache.commons.lang3.StringUtils的依赖 可以实现和上面循环一样的拼接效果
+        String turnoverListString = StringUtils.join(turnoverList, ",");
+        return new TurnoverReportVO(dateListString, turnoverListString);
+    }
+}
