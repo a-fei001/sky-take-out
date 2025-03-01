035af7dc92b1cf9f97da3e9b507a00e6a3dcd90c
a-fei <sure_0725@qq.com>
2025-03-01 16:51:41 +0800

    day12-工作台

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -13,6 +13,7 @@ import org.apache.ibatis.annotations.Update;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface DishMapper {
@@ -53,4 +54,11 @@ public interface DishMapper {
     List<Dish> selectBySetmealId(Long setmealId);
 
     List<Dish> list(Dish dish);
+
+    /**
+     * 根据条件统计菜品数量
+     * @param map
+     * @return
+     */
+    Integer countByMap(Map map);
 }
